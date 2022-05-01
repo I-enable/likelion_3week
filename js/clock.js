c60f2e1e7e5f61d0e002808ba741827f2dd9fcc8
tlawogud <tlawogud12@naver.com>
2022-05-01 15:38:55 +0900

    default

--- js/clock.js
+++ js/clock.js
@@ -2,7 +2,7 @@ const clock = document.querySelector("h2#clock");
 
 function getClock() {
   //[quiz] 'Date() 생성자'에 대하여 검색하기 
-  const date = ___ _____();
+  const date = new Date();
   const hours = String(date.getHours()).padStart(2, "0");
   const minutes = String(date.getMinutes()).padStart(2, "0");
   const seconds = String(date.getSeconds()).padStart(2, "0");
